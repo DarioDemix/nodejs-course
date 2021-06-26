9750700180868f9fb38a924d3adbc681d89d1e92
Dario De Marco <dario.demarco1208@gmail.com>
2021-06-26 17:04:08 +0200

    Saved products with FS

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -10,17 +10,17 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const { title } = req.body;
   const product = new Product(title, 19.99, "Lorem ipsum");
-  
+
   product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-
-  res.render("shop", {
-    products,
-    docTitle: "My Shop",
-    path: "/",
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      products,
+      docTitle: "My Shop",
+      path: "/",
+    });
   });
 };
--- models/product.js
+++ models/product.js
@@ -1,15 +1,35 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
 
+const productPath = path.join(rootDir, `data`, `products.json`);
 module.exports = class Product {
-    constructor(title, price, description) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-    }
+  constructor(title, price, description) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    fs.readFile(productPath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(productPath, JSON.stringify(products), (e) =>
+        console.log(e)
+      );
+    });
+  }
 
-    static fetchAll() { return products; }
-}
+  static fetchAll(callback) {
+    fs.readFile(productPath, (err, fileContent) => {
+      if (err) {
+        callback([]);
+      } else {
+        callback(JSON.parse(fileContent));
+      }
+    });
+  }
+};
