7ead5f8ac34d057b8d21f516156ce878f31c2a6a
Dario De Marco <dario.demarco1208@gmail.com>
2021-06-26 19:34:06 +0200

    Added orders path and view

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop-controller");
-const { getProducts, getCart, getIndex, getCheckout } = shopController;
+const { getProducts, getCart, getIndex, getCheckout, getOrders } = shopController;
 
 router.get("/", getIndex);
 
 router.get("/products", getProducts);
 
 router.get("/cart", getCart);
+
+router.get("/orders", getOrders);
 
 router.get("/checkout", getCheckout);
 
