cefe7a4b5d998878e50e7ffd6b4c7410d89fa9dc
Dario De Marco <dario.demarco1208@gmail.com>
2021-06-27 17:54:00 +0200

    recreated Product model with sequelize

--- models/product.js
+++ models/product.js
@@ -1,31 +1,29 @@
-const Cart = require('./Cart');
-const db = require('../util/database');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-   return db.query(
-      'INSERT INTO products (title, price, "imageUrl", description) VALUES ($1, $2, $3, $4)', // Prevent SQL Injection
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static fetchAll() {
-    return db.query('SELECT * FROM products');
-  }
-
-  static findById(id, cb) {
-    return db.query('SELECT * FROM products WHERE id = $1', [id]);
-  }
-
-  static deleteById(id, cb) {
-   
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,15 +1,17 @@
 require('./env');
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const {
     PGUSER,
     PGPASSWORD,
     PGDATABASE,
+    PGPORT
 } = process.env;
 
 const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, { 
     dialect: 'postgres',
-    host: 'localhost'
+    host: 'localhost',
+    port: PGPORT
 });
 
 module.exports = sequelize;
