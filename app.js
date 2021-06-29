d6bf643a67c595044e2b2ddc5611332ce7707e42
Dario De Marco <demarcod@areariscossioni.it>
2021-06-29 19:49:13 +0200

    Switched Product with User in app.js to respect the association

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const errorController = require('./controllers/error-controller');
-const navigationController = require('./controllers/navigation-controller');
+const errorController = require("./controllers/error-controller");
+const navigationController = require("./controllers/navigation-controller");
 
-const sequelize = require('./util/database');
-const Product = require('./models/Product');
-const User = require('./models/User');
+const sequelize = require("./util/database");
+const Product = require("./models/Product");
+const User = require("./models/User");
 
 const app = express();
 
@@ -27,11 +27,12 @@ app.use(express.static(path.join(__dirname, `public`)));
 app.use(navigationController.setLinksMiddleware);
 
 app.use((req, res, next) => {
-    User.findByPk(1).then(user => {
-        req.user = user;
-        next();
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -39,23 +40,23 @@ app.use(shopRoutes);
 
 app.use(errorController.getNotFound);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 // Associations doesn't work if sequelize.sync is used. You need to use the sync method on the individual models.
-Promise.all([Product.sync(), User.sync()])
-    .then(() => User.findByPk(1))
-    .then(user => {
-        if (!user) { 
-            return User.create({ name: 'Dario', email: 'test@testmail.io'});
-        }
-        return Promise.resolve(user);    
-    })
-    .then(user => {
 
-        console.log(user.dataValues);
+Promise.all([User.sync(), Product.sync()])
+  .then(() => User.findByPk(1))
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Dario", email: "test@testmail.io" });
+    }
+    return Promise.resolve(user);
+  })
+  .then((user) => {
+    console.log(user.dataValues);
 
-        app.listen(port);
-        console.log(`Server created at http://localhost:${port}`);
-    })
-    .catch(err => console.log(err));
+    app.listen(port);
+    console.log(`Server created at http://localhost:${port}`);
+  })
+  .catch((err) => console.log(err));
